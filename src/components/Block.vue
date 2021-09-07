3ed92daf4625ab40b88857c52af304902b15c7bd
ethilty <ethilty@crimson.ua.edu>
2021-09-07 14:13:33 -0500

    Block, BlockPicker

---
+++ src/components/Block.vue
@@ -0,0 +1,25 @@
+<script>
+import BlockType from '@/services/BlockType'
+
+export default {
+    name: 'Block',
+
+    props: {
+        type: BlockType
+    },
+
+    render(create) {
+        // Create block element
+        return create(
+            'div',
+            {
+                class: 'block',
+                style: {
+                    color: this.type.color,
+                }
+            }
+        )
+    }
+
+}
+</script>
---
+++ src/components/BlockPicker.vue
@@ -0,0 +1,23 @@
+<script>
+import BlockType from '@/services/BlockType'
+import Block from './Block'
+
+export default {
+    name: 'BlockPicker',
+    
+    render(create) {
+        // Create a div with one block for each block type as children
+        return create(
+            'div',
+            {
+                class: 'block-picker',
+            },
+            BlockType.values.map((type) => create(Block, {
+                props: {
+                    type: type
+                }
+            }))
+        )
+    }
+}
+</script>
