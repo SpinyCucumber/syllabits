c6dfcb293e92777ca49814f38c3f7e820c9982a3
ethilty <ethilty@crimson.ua.edu>
2021-09-07 14:22:14 -0500

    Babel

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
         <h1>TODO</h1>
+        <block-picker/>
     </div>
 </template>
+
+<script>
+import { BlockPicker } from '@/components'
+
+export default {
+    name: 'Dashboard',
+    components: { BlockPicker }
+}
+</script>
