6b972193de19296bf94a179a830398cedcf1d325
ethilty <ethilty@crimson.ua.edu>
2021-09-15 00:20:25 -0500

    Scrolling

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,13 +1,13 @@
 <template>
     <!-- TODO Use SASS for background color, etc. -->
     <div
-        class="has-background-primary"
-        style="display: flex; flex-direction: column; position: relative; align-items: center; justify-content: middle;">
+        style="display: flex; flex-direction: column; position: relative;
+            align-items: stretch; min-height: 0;">
 
         <b-dropdown
             animation="slide"
             class="block-dropdown"
-            style="position: absolute; top: 0px; left: 1em;"
+            style="position: absolute; top: 0em; left: 1em;"
 
             :can-close="false"
             :close-on-click="false">
@@ -22,31 +22,44 @@
             </b-dropdown-item>
         </b-dropdown>
 
-        <div v-if="poem"
-            class="has-background-white poem"
-            style="display: flex; flex-grow: 1; flex-direction: column; align-items: center; gap: 4em;
-                padding: 2em 6em 2em 6em;">
+        <div
+            class="has-background-primary"
+            style="display: flex; flex-direction: column; flex-grow: 1; min-height: 0;
+                align-items: center; justify-content: middle; overflow-y: scroll;">
+            <div style="display: flex; flex-direction:column; flex-grow: 1;" class="has-background-white">
 
-            <div style="display: flex; flex-direction: column; align-items: center;">
-                <div class="title">{{ poem.name }}</div>
-                <div class="author">{{ poem.author }}</div>
-            </div>
-            
-            <svg width="400" height="8">
-                <line class="divider" x1="4" y1="4" x2="396" y2="4"/>
-            </svg>
-            
-            <!-- Poem lines -->
-            <div style="display: flex; flex-grow: 1; flex-direction: column; gap: 2em;">
-                 <div
-                    v-for="line in poem.lines"
-                    :key="line.number"
-                    style="display: flex; align-items: center; gap: 4em;">
-                    <!-- Line number -->
-                    <div class="line-number">{{ line.number }}</div>
-                    <!-- Line component -->
-                    <poem-line :line="line"/>
-                 </div>
+                <div style="display: flex; justify-content: flex-end; gap: 1em; padding: 1em;">
+                    <b-button type="is-primary is-light" icon-right="help"/>
+                </div>
+
+                <div v-if="poem"
+                    class="poem"
+                    style="display: flex; flex-grow: 1; flex-direction: column; align-items: center; gap: 4em;
+                        padding-left: 4em; padding-right: 4em;">
+
+                    <div style="display: flex; flex-direction: column; align-items: center;">
+                        <div class="title">{{ poem.name }}</div>
+                        <div class="author">{{ poem.author }}</div>
+                    </div>
+                    
+                    <svg width="400" height="8">
+                        <line class="divider" x1="4" y1="4" x2="396" y2="4"/>
+                    </svg>
+                    
+                    <!-- Poem lines -->
+                    <div style="display: flex; flex-grow: 1; flex-direction: column; gap: 2em;">
+                        <div
+                            v-for="line in poem.lines"
+                            :key="line.number"
+                            style="display: flex; align-items: center; gap: 4em;">
+                            <!-- Line number -->
+                            <div class="line-number">{{ line.number }}</div>
+                            <!-- Line component -->
+                            <poem-line :line="line"/>
+                        </div>
+                    </div>
+
+                </div>
             </div>
 
         </div>
