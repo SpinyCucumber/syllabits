98c71679bb18447557efb634cf9c70c6889878dc
ethilty <ethilty@crimson.ua.edu>
2021-09-18 01:49:53 -0500

    Dovestype

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import Buefy from 'buefy'
 
 import 'buefy/dist/buefy.css'
+import './styles/DovesType-Regular.css'
 import './styles/syllabits.css'
 import '@mdi/font/css/materialdesignicons.min.css'
 import { createProvider } from './vue-apollo'
