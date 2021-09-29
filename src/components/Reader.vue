786b71f3a2aa910a1bb69985fccc38a0f3346543
ethilty <ethilty@crimson.ua.edu>
2021-09-29 16:01:16 -0500

    Reader.vue

---
+++ src/components/Reader.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="reader">
+        <div class="static-area"><slot name="static-area"/></div>
+        <div class="scroll-area">
+            <div class="background">
+                <div class="content-area"><slot name="content-area"/></div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Block } from './Block';
 export { default as BlockSlot } from './BlockSlot';
 export { default as BlockPicker } from './BlockPicker';
 export { default as PoemLine } from './PoemLine';
+export { default as Reader } from './Reader';
