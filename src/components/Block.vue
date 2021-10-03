6bd1e1c2fab5b1e009bae016c09bcee2eb5a58ef
ethilty <ethilty@crimson.ua.edu>
2021-10-02 19:37:34 -0500

    Removed inline style from Block

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,8 +1,7 @@
 <template>
     <div
         class="game-block"
-        :style="{ 'background-color': this.$config.getBlockTypeColor(this.type),
-            display: 'flex', 'align-items': 'center', 'justify-content': 'space-around', padding: '0.5em' }">
+        :style="{ 'background-color': this.$config.getBlockTypeColor(this.type) }">
         <img v-for="(stress, index) in this.type.stresses" :src="$assets.getIcon(stress.name)" :key="index" class="game-notation"/>
     </div>
 </template>
