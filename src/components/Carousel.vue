bb496daee77c66aed31f2dc0a4fb1bfd13b1b778
ethilty <spinycucumber@gmail.com>
2021-10-20 17:42:14 -0500

    Began work on Carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'Carousel',
+    props: {
+        imageIDs: { require: true },
+    },
+    data() {
+        return {
+            index: 0,
+        }
+    },
+}
+</script>
--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as PoemLine } from './PoemLine';
 export { default as Reader } from './Reader';
 export { default as GameProgress } from './GameProgress';
 export { default as GameDropdown } from './GameDropdown';
+export { default as Carousel } from './Carousel';
