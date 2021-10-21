e158e6c80691b404ba6f6c6f96404380befcc217
ethilty <spinycucumber@gmail.com>
2021-10-20 23:01:49 -0500

    Reader fixes

--- src/components/Reader.vue
+++ src/components/Reader.vue
@@ -2,16 +2,16 @@
     <div class="reader">
         <div class="static-area"><slot name="static-area"/></div>
         <div class="scroll-area">
-            <div class="background-area">
-                <slot name="background-area">
-                    <!-- Default background -->
-                    <background-image
-                        :src="$assets.getTexture('Paper2')"
-                        class="warm"/>
-                </slot>
-            </div>
-            <div class="content-area">
-                <div class="inner">
+            <div class="inner">
+                <div class="background-area">
+                    <slot name="background-area">
+                        <!-- Default background -->
+                        <background-image
+                            :src="$assets.getTexture('Paper2')"
+                            class="warm"/>
+                    </slot>
+                </div>
+                <div class="content-area">
                     <slot name="content-area"/>
                 </div>
             </div>
