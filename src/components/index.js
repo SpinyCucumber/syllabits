56cdfbcaac6dc93e4633a5e5efeed4bb7619a4ff
ethilty <ethilty@crimson.ua.edu>
2021-10-21 00:51:10 -0500

    layout subfolder

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,7 @@
-export { default as Block } from './Block';
-export { default as BlockSlot } from './BlockSlot';
-export { default as BlockPicker } from './BlockPicker';
-export { default as PoemLine } from './PoemLine';
-export { default as Reader } from './Reader';
-export { default as GameProgress } from './GameProgress';
-export { default as GameDropdown } from './GameDropdown';
-export { default as Carousel } from './Carousel';
-export { default as BackgroundImage } from './BackgroundImage';
+export { default as Block } from './Block'
+export { default as BlockSlot } from './BlockSlot'
+export { default as BlockPicker } from './BlockPicker'
+export { default as PoemLine } from './PoemLine'
+export { default as GameProgress } from './GameProgress'
+export { default as GameDropdown } from './GameDropdown'
+export * from './layout'
---
+++ src/components/layout/index.js
@@ -0,0 +1,3 @@
+export { default as Reader } from './Reader'
+export { default as Carousel } from './Carousel'
+export { default as BackgroundImage } from './BackgroundImage'
