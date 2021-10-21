6afcc868c88db3e6088ebc7f812a38f568166558
ethilty <ethilty@crimson.ua.edu>
2021-10-21 12:39:43 -0500

    Splash content

--- src/views/Splash.vue
+++ src/views/Splash.vue
@@ -1,7 +1,15 @@
 <template>
     <reader class="splash" :card="true">
         <template #content-area>
-            <h1 class="branding">Syllabits</h1>
+            <div class="title-box">
+                <h1 class="branding">SyllaBits</h1>
+                <img :src="$assets.getIcon('Divider')" class="divider"/>
+                <poem-line
+                    v-for="(line, i) in demoPoem"
+                    :key="i"
+                    :line="line"
+                    :lineProgressProxy="demoProgress[i]"/>
+            </div>
         </template>
         <template #background-area>
             <carousel :imageSources="images"/>
@@ -10,14 +18,19 @@
 </template>
 
 <script>
-import { Reader, Carousel } from '@/components'
+import { Reader, Carousel, PoemLine } from '@/components'
 import { AssetService } from '@/services'
 
 export default {
-    components: { Reader, Carousel },
+    components: { Reader, Carousel, PoemLine },
     data() {
         return {
-            images: ['Paper1', 'Paper2', 'Paper3', 'Paper4'].map(AssetService.getTexture)
+            images: ['Paper1', 'Paper2', 'Paper3', 'Paper4'].map(AssetService.getTexture),
+            demoPoem: [
+                { text: "With building blocks to practice and refine" },
+                { text: "your scansion skills will grow with every line" },
+            ],
+            demoProgress: []
         }
     }
 }
