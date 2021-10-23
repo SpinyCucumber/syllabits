4a5bf2e7dfe6839ddce15c805e2bb4aa087001ad
ethilty <spinycucumber@gmail.com>
2021-10-23 06:19:04 -0500

    Smarter import-poems

--- scripts/import-poems.js
+++ scripts/import-poems.js
@@ -21,6 +21,12 @@ const SYMBOL_TABLE = {
     'uu': 'p',
 }
 
+const LINE_LENGTH = 5;
+const DIV_SMALL = '===================================================';
+const DIV_LARGE = '\n';
+
+const FALLBACK_BLOCK = 'i';
+
 function parseStanzas(stanzaStr) {
     return stanzaStr.split(',').map(str => parseInt(str));
 }
@@ -33,21 +39,41 @@ function parsePoem(worksheet) {
     let data = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
 
     let lines = [];
+    let lineIndex = 0;
+    let numErrors = 0;
     for(let i = 0; i < data.length; i += 2) {
         // Skip empty line
         if (data[i].length == 0) continue;
         // Read in array of block types, then convert to code string
-        const lineKey = data[i].map(sym => SYMBOL_TABLE[sym]).join('');
-        const lineText = data[i+1].slice(0, lineKey.length).join(' ');
+        const lineKey = data[i].slice(0, LINE_LENGTH).map((sym, symIndex) => {
+            const blockCode = SYMBOL_TABLE[sym];
+            if (!blockCode) {
+                console.error(`Invalid stress pattern: ${sym} (Line: ${lineIndex+1}) (Foot: ${symIndex+1})`);
+                numErrors += 1;
+                return FALLBACK_BLOCK;
+            }
+            return blockCode;
+        }).join('');
+        // Check line length
+        if (lineKey.length !== LINE_LENGTH) {
+            console.error(`Invalid line length: ${lineKey.length} (Line: ${lineIndex})`);
+            numErrors += 1;
+        }
+        const lineText = data[i+1].slice(0, LINE_LENGTH).join(' ');
         // Construct line
         const line = { text: lineText, key: lineKey };
         lines.push(line);
+        lineIndex += 1;
     }
     // Construct poem
-    return { lines };
+    return [{ lines }, numErrors];
 }
 
 function importPoems(inputPath, author, stanzas) {
+
+    // Output status
+    console.log(`Importing poems from: ${inputPath}`);
+    console.log(DIV_LARGE);
 
     // Load workbook from unprocessed poem file
     const workbook = xlsx.readFile(inputPath);
@@ -58,28 +84,46 @@ function importPoems(inputPath, author, stanzas) {
         stanzaBreaks.push(line);
         line += stanzaSize;
     }
+    const totalLines = line;
 
+    let totalErrors = 0;
     const poems = workbook.SheetNames
         .map(name => {
-            let poem = parsePoem(workbook.Sheets[name]);
-            poem.name = `Sonnet ${name}`;
-            poem.author = author;
-            poem.id = crypto.randomBytes(8).toString('hex');
-            // Update stanzas
-            // Don't update if we have an irregular number of lines
-            if (poem.lines.length == line) {
-                for (let stanzaBreak of stanzaBreaks) {
-                    poem.lines[stanzaBreak].stanzaBreak = true;
+                let [poem, numErrors] = parsePoem(workbook.Sheets[name]);
+                // Check number of lines; if invalid, throw error
+                // If valid, we can mark stanza breaks
+                if (poem.lines.length !== totalLines) {
+                    console.error(`Invalid line count: ${poem.lines.length}`);
+                    numErrors += 1;
                 }
-            }
-            return poem;
-        });
+                else {
+                    for (let stanzaBreak of stanzaBreaks) {
+                        poem.lines[stanzaBreak].stanzaBreak = true;
+                    }
+                }
+                poem.name = `Sonnet ${name}`;
+                poem.author = author;
+                poem.id = crypto.randomBytes(8).toString('hex');
+                // Output poem-specific error statistics
+                if (numErrors > 0) {
+                    console.error(`Encountered ${numErrors} error(s) while processing poem: ${name}`);
+                    console.error(DIV_SMALL);
+                }
+                totalErrors += numErrors;
+
+                return poem;
+            });
+
+    // Output statistics
+    console.log(DIV_LARGE);
+    console.log('Done!');
+    console.log(`Total poems: ${poems.length}`);
+    console.log(`Total errors: ${totalErrors}`);
 
     // Dump to processed poems
     const data = JSON.stringify(poems, null, 2);
     fs.writeFile(PROCESSED_POEMS_PATH, data, err => {
         if (err) throw err;
-        console.log('Imported poems.');
     });
 
 }
