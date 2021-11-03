f7739b08a71422ae45ddc3913cd234a88cdce5f6
ethilty <ethilty@crimson.ua.edu>
2021-11-03 17:10:27 -0500

    Login query

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,12 +3,12 @@
         <template #content-area>
             <!-- TODO Could change logo here (or background) to something more exciting -->
             <img :src="$assets.getTexture('Logo')" class="logo"/>
-            <form>
+            <form @submit.prevent="submit">
                 <b-field label="Email" label-position="on-border">
-                    <b-input type="email" v-model="inputs.email"/>
+                    <b-input type="email" v-model="input.email"/>
                 </b-field>
                 <b-field label="Password" label-position="on-border">
-                    <b-input type="password" v-model="inputs.password"/>
+                    <b-input type="password" v-model="input.password"/>
                 </b-field>
                 <b-button tag="input" type="is-primary" native-type="submit" value="Login"/>
             </form>
@@ -18,14 +18,29 @@
 
 <script>
 import { Reader } from '@/components'
+import { login as loginQuery } from '@/queries'
 
 export default {
     name: 'Login',
     components: { Reader },
     data() {
         return {
-            inputs: {}
+            input: {},
+            busy: false,
         }
     },
+    methods: {
+        submit() {
+            this.busy = true;
+            // Submit input to server
+            this.$apollo.mutate({ mutation: loginQuery, variables: { input: this.input } })
+                .then(result => result.data.login.token)
+                .then(token => {
+                    this.busy = false;
+                    // TODO
+                    console.log(token);
+                });
+        }
+    }
 }
 </script>
