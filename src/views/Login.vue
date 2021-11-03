cd263faeed48288a47fe98ab3f12e7f49366307a
ethilty <ethilty@crimson.ua.edu>
2021-11-03 16:56:53 -0500

    Login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,18 @@
 <template>
-    <reader class="centered">
-        TODO
+    <reader class="login centered">
+        <template #content-area>
+            <!-- TODO Could change logo here (or background) to something more exciting -->
+            <img :src="$assets.getTexture('Logo')" class="logo"/>
+            <form>
+                <b-field label="Email" label-position="on-border">
+                    <b-input type="email" v-model="inputs.email"/>
+                </b-field>
+                <b-field label="Password" label-position="on-border">
+                    <b-input type="password" v-model="inputs.password"/>
+                </b-field>
+                <b-button tag="input" type="is-primary" native-type="submit" value="Login"/>
+            </form>
+        </template>
     </reader>
 </template>
 
@@ -9,6 +21,11 @@ import { Reader } from '@/components'
 
 export default {
     name: 'Login',
-    components: { Reader }
+    components: { Reader },
+    data() {
+        return {
+            inputs: {}
+        }
+    },
 }
 </script>
