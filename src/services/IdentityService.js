7510fe90245834d2b30aed0ed944f2cb6702ffce
ethilty <ethilty@crimson.ua.edu>
2021-11-05 00:27:00 -0500

    IdentityService exp handler

--- src/services/IdentityService.js
+++ src/services/IdentityService.js
@@ -1,9 +1,18 @@
 // Handles tracking user identity.
 // This handling JWTs as well as things like admin status.
 import Service from './Service'
+import jwt_decode from 'jwt-decode'
+
+const PREEMPT = 10000;
+let refreshTimeout = null;
 
 let state = {
     token: null
+}
+
+function refreshIdentity() {
+    // TODO
+    state.token = null;
 }
 
 export default new Service({
@@ -17,6 +26,12 @@ export default new Service({
 
     setIdentity(token) {
         state.token = token
+        const claims = jwt_decode(token);
+        // Schedule token refresh
+        // Make sure to cancel previously scheduled refresh if applicable
+        const delta = (claims.exp * 1000) - Date.now() - PREEMPT;
+        if (refreshTimeout) clearTimeout(refreshTimeout);
+        refreshTimeout = setTimeout(refreshIdentity, delta);
     }
     
 });
