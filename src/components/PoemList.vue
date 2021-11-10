bdabb28eb54dcd743cfa60c5c4ccdcfa1b158a75
ethilty <spinycucumber@gmail.com>
2021-11-09 23:18:05 -0600

    Dashboard, PoemList, inProgress query

---
+++ src/components/PoemList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="poem-list">
+        TODO
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PoemList',
+    props: {
+        query: { required: true },
+    },
+    apollo: {
+        poems: {
+            query() {
+                return this.query;
+            }
+        }
+    }
+}
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,35 @@
 <template>
-    <div>
-        <h1>TODO</h1>
+    <div class="dashboard">
+        <div
+            v-for="widget in widgets"
+            :key="widget.key"
+            class="widget">
+            <h3>{{ $translation.get('widget.' + widget.key) }}</h3>
+            <component
+                :is="widget.component"
+                v-bind="widget.props"
+                class="inner"/>
+        </div>
     </div>
 </template>
 
 <script>
+import { PoemList } from '@/components'
+import { inProgress as inProgressQuery } from '@/queries'
 
 export default {
     name: 'Dashboard',
+
+    data() {
+        return {
+            widgets: [
+                {
+                    key: 'resume',
+                    component: PoemList,
+                    props: { query: inProgressQuery },
+                }
+            ]
+        }
+    }
 }
 </script>
