3e75752d63ff0eac2e30b8738ace2bef9dfb5ab0
ethilty <ethilty@crimson.ua.edu>
2021-11-11 18:09:22 -0600

    Dashboard background

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,24 +1,30 @@
 <template>
-    <div class="dashboard">
-        <div
-            v-for="widget in widgets"
-            :key="widget.key"
-            class="widget">
-            <h3 class="widget-title">{{ $translation.get('widget.' + widget.key) }}</h3>
-            <component
-                :is="widget.component"
-                v-bind="widget.props"
-                class="inner"/>
-        </div>
-    </div>
+    <reader class="dashboard">
+        <template #content-area>
+            <div
+                v-for="widget in widgets"
+                :key="widget.key"
+                class="widget">
+                <h3 class="widget-title">{{ $translation.get('widget.' + widget.key) }}</h3>
+                <component
+                    :is="widget.component"
+                    v-bind="widget.props"
+                    class="inner"/>
+            </div>
+        </template>
+        <template #background-area>
+            <background-image :src="$assets.getTexture('Books')" class="muted"/>
+        </template>
+    </reader>
 </template>
 
 <script>
-import { PoemList } from '@/components'
+import { PoemList, Reader, BackgroundImage } from '@/components'
 import { inProgress as inProgressQuery } from '@/queries'
 
 export default {
     name: 'Dashboard',
+    components: { Reader, BackgroundImage },
 
     data() {
         return {
