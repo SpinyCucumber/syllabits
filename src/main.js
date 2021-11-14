27c6a7dcdb1fc596ec22221527f78a8cdbb1cd30
ethilty <spinycucumber@gmail.com>
2021-11-14 16:48:18 -0600

    IdentityService uses vuex

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import Buefy from 'buefy'
-
-import './styles/syllabits.scss'
 
 import { createProvider } from './apollo'
 import { Constants, IdentityService, AssetService, TranslationService } from './services'
-import store from './store'
+
+import './styles/syllabits.scss'
 
 // Install Vue plugins!
 // Buefy bundle size is actually small
 Vue.use(Buefy);
 Vue.use(Constants);
-Vue.use(IdentityService);
+Vue.use(IdentityService, {store});
 Vue.use(AssetService);
 Vue.use(TranslationService);
 
--- src/services/IdentityService.js
+++ src/services/IdentityService.js
@@ -4,34 +4,58 @@ import Service from './Service'
 import jwt_decode from 'jwt-decode'
 
 const PREEMPT = 10000;
-let refreshTimeout = null;
 
-let state = {
-    identity: null
-}
-
-function refreshIdentity() {
-    // TODO
-    state.identity = null;
-}
-
-export default new Service({
-
-    name: 'identity',
-    state,
-
-    hasIdentity() {
-        return Boolean(state.identity)
+const module = {
+    state() {
+        return {
+            token: null,
+            claims: null,
+            refreshTimeout: null,
+        }
     },
-
-    setIdentity(identity) {
-        state.identity = identity
-        const claims = jwt_decode(identity);
-        // Schedule token refresh
-        // Make sure to cancel previously scheduled refresh if applicable
-        const delta = (claims.exp * 1000) - Date.now() - PREEMPT;
-        if (refreshTimeout) clearTimeout(refreshTimeout);
-        refreshTimeout = setTimeout(refreshIdentity, delta);
+    mutations: {
+        setToken(state, token) {
+            state.token = token;
+        },
+        setClaims(state, claims) {
+            state.claims = claims;
+        },
+        setRefreshTimeout(state, refreshTimeout) {
+            state.refreshTimeout = refreshTimeout;
+        }
+    },
+    actions: {
+        load({state, commit, dispatch}, token) {
+            const claims = jwt_decode(token);
+            // Schedule token refresh
+            // Make sure to cancel previously scheduled refresh if applicable
+            const delta = (claims.exp * 1000) - Date.now() - PREEMPT;
+            if (state.refreshTimeout) clearTimeout(state.refreshTimeout);
+            const refreshTimeout = setTimeout(() => dispatch('refreshIdentity'), delta);
+            // Commit mutations
+            commit('setToken', token);
+            commit('setClaims', claims);
+            commit('setRefreshTimeout', refreshTimeout);
+        },
+        refreshIdentity() {
+            // TODO
+            commit('setToken', null);
+        }
     }
-    
-});
+}
+
+class IdentityService extends Service {
+
+    constructor() {
+        super({name: 'identity'});
+    }
+
+    install(vue, options) {
+        // Must pass store as option
+        options.store.registerModule('identity', module);
+        super.install(vue);
+    }
+
+}
+
+export default new IdentityService();
