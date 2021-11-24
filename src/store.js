a9735a99c89ed67d99cb521af70bdb0a14173dce
ethilty <spinycucumber@gmail.com>
2021-11-24 01:16:50 -0600

    use vuexPersist

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { getField, updateField } from 'vuex-map-fields';
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
 
@@ -18,6 +19,15 @@ const settings = {
     mutations: { updateField },
 }
 
+// Construct persist plugin
+// We only persist settings, as the refresh token is stored in cookies
+// TODO Look into
+const vuexLocal = new VuexPersistence({
+    storage: window.localStorage,
+    modules: ['settings'],
+})
+
 export default new Vuex.Store({
-    modules: { settings }
+    modules: { settings },
+    plugins: [vuexLocal.plugin],
 })
