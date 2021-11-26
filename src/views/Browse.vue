68c9e445d7d5d7ca0ee02afe7712892e8f996ecd
ethilty <spinycucumber@gmail.com>
2021-11-26 00:15:19 -0600

    Browse vue initial logic

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -1,14 +1,42 @@
 <template>
   <scene>
-
+    <template #content-area>
+        <!-- Testing -->
+        <connection
+            :query="browsePoemsQuery"
+            :component="PoemEntry"
+            field="allPoems"
+            prop="poem"
+            :perPage="10"/>
+    </template>
+    <template #background-area>
+        <background-image :src="$assets.getTexture('Books')" class="muted"/>
+    </template>
   </scene>
 </template>
 
 <script>
-import { Scene } from '@/components'
+import { Scene, BackgroundImage, Connection } from '@/components'
+import { browsePoems as browsePoemsQuery } from '@/queries'
+import Vue from 'vue'
+
+// Define components used to render lists
+const PoemEntry = Vue.component('PoemEntry', {
+    props: { poem: {required: true} },
+    render() {
+        return (
+            <tr>
+                <td>{this.poem.title}</td>
+            </tr>
+        )
+    }
+})
 
 export default {
     name: 'Browse',
-    components: { Scene }
+    components: { Scene, BackgroundImage, Connection },
+    setup() {
+        return { PoemEntry, browsePoemsQuery };
+    }
 }
 </script>
