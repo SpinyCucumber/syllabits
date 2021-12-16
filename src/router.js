42bb22e49f6f4b970c9d063abd8a256a74f8bc3c
ethilty <spinycucumber@gmail.com>
2021-12-16 12:12:33 -0600

    Use PoemLocation in randompoem route

--- src/router.js
+++ src/router.js
@@ -3,8 +3,11 @@ import VueRouter from 'vue-router'
 import store from '@/store'
 import { Splash, Login, Register, Dashboard, Play, Browse } from '@/views'
 import { randomPoemID as randomPoemIDQuery } from '@/queries'
-import { ReminderService } from '@/services'
+import { ReminderService, Constants } from '@/services'
+import { PoemLocation } from '@/utilities'
 import { apolloClient } from '@/apollo'
+
+const { LocationType } = Constants;
 
 Vue.use(VueRouter)
 
@@ -30,7 +33,8 @@ routes: [
         .then(result => result.data.randomPoem.poem.id)
         .then(id => {
           // Transition to play page with the new poem ID
-          next({ name: 'Play', params: {location: `poem/${id}`}});
+          const location = new PoemLocation({t: LocationType.DIRECT, pid: id}).encode();
+          next({ name: 'Play', params: {location}});
         });
     },
   },
