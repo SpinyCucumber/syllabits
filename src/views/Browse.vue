01671ad2a4278041d71bc29f5329868ae2efe463
ethilty <spinycucumber@gmail.com>
2021-12-18 01:27:11 -0600

    Use buefy table in Browse

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -1,50 +1,25 @@
 <template>
-  <scene type="is-aligned">
-    <template #content-area>
-        <!-- Testing -->
-        <div class="scene-content">
-            <!-- TODO Use buefy table -->
-            <table class="poem-entry-list">
-                <poem-entry v-for="poem in entries"
-                    :key="poem.id"
-                    :poem="poem"/>
-            </table>
-        </div>
-    </template>
-    <template #background-area>
-        <background-image :src="$assets.getTexture('Books')" class="muted"/>
-    </template>
-  </scene>
+    <div>
+        <!-- TODO Use buefy table -->
+        <b-table :data="entries" :columns="columns" hoverable/>
+    </div>
 </template>
 
 <script>
-import { Scene, BackgroundImage } from '@/components'
 import { browsePoems as browsePoemsQuery } from '@/queries'
+import { TranslationService } from '@/services'
 import { Connection } from '@/mixins'
-import Vue from 'vue'
-
-// Define components used to render lists
-const PoemEntry = Vue.component('PoemEntry', {
-    props: { poem: {required: true} },
-    render() {
-        return (
-            <tr>
-                <td>{this.poem.title}</td>
-                <td>{this.poem.author}</td>
-            </tr>
-        )
-    }
-})
 
 export default {
     name: 'Browse',
-    components: { Scene, BackgroundImage, PoemEntry },
     mixins: [ Connection('entries', {
         query: browsePoemsQuery,
         update: data => data.allPoems,
     }) ],
     setup() {
-        return { PoemEntry, browsePoemsQuery };
+        const fields = ['title', 'author'];
+        const columns = fields.map(field => ({field, label: TranslationService.get('field.' + field)}));
+        return { columns };
     }
 }
 </script>
