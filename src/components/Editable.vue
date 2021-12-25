e3f246a9b7542910638a1f8f585679927b79586b
ethilty <spinycucumber@gmail.com>
2021-12-25 17:23:30 -0600

    Editable

---
+++ src/components/Editable.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-input v-if="editable" v-bind="{value} = $attrs" v-on="{input} = $listeners"/>
+  <div v-else>
+    <slot/>
+  </div>
+</template>
+
+<script>
+/**
+ * A component meant to wrap a editable field.
+ * The ability to edit can be toggled on/off.
+ */
+export default {
+    name: 'Editable',
+    props: {
+        value: { required: true },
+        editable: { default: false },
+    }
+}
+</script>
