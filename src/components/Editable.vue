7f808aa86a168b5f756b5f199f6a3a88b7a3a88b
ethilty <spinycucumber@gmail.com>
2021-12-26 01:04:00 -0600

    Documentation in Editable

--- src/components/Editable.vue
+++ src/components/Editable.vue
@@ -1,5 +1,5 @@
 <template>
-  <component v-if="editable"
+  <component v-if="allowEditing"
     :is="inputType"
     v-bind="$attrs"
     v-on="$listeners"/>
@@ -20,27 +20,36 @@ const inputTypeLookup = {
  * The ability to edit can be toggled on/off.
  */
 export default {
+
     name: 'Editable',
     inheritAttrs: false,
+
     props: {
         type: { default: 'text' },
         tag: { default: 'span' },
     },
+
     computed: {
       inputType() {
         return inputTypeLookup[this.type];
       }
     },
+    /**
+     * Each editable component looks for nearest ancestor with the 'allowEditing' attribute,
+     * and inherits it if it can. This creates a sort of "editing context" and makes uses the
+     * component much cleaner. (Avoids passing props everywhere)
+     */
     beforeCreate() {
       let c = this;
       while (c) {
         if (c.allowEditing !== undefined) {
-          this.editable = c.allowEditing;
+          this.allowEditing = c.allowEditing;
           return;
         }
         c = c.$parent;
       }
-      this.editable = false;
+      this.allowEditing = false;
     },
+    
 }
 </script>
