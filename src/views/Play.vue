d9ff6be7967b61f77b3e9dd915bd163b753efccf
ethilty <spinycucumber@gmail.com>
2021-12-28 17:36:49 -0600

    Final excludeFields lsit

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -140,7 +140,7 @@ export default {
         TrackChanges({
             prop: 'poem',
             original: 'original',
-            excludeFields: ['id', 'progress']
+            excludeFields: ['id', 'progress', '__typename', 'location']
         }),
     ],
 
