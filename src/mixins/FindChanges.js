116bfafa92d7ec0a43121f1ff22830786dc1bec8
ethilty <ethilty@crimson.ua.edu>
2022-01-10 15:55:31 -0600

    documentListHandler

--- src/mixins/FindChanges.js
+++ src/mixins/FindChanges.js
@@ -38,7 +38,12 @@ const documentHandler = new Handler({
             if (hint) {
                 let handler = handlerForHint(hint);
                 let fieldPath = path ? path + '.' + fieldName : fieldName;
-                yield* handler.findChanges({path: fieldPath, newValue: newFieldValue, oldValue: oldFieldValue});
+                yield* handler.findChanges({
+                    path: fieldPath,
+                    newValue: newFieldValue,
+                    oldValue: oldFieldValue,
+                    context,
+                });
             }
             // If the field is an atomic type (e.g. String, Number) or explicitly marked as atomic,
             // we check for equality between the old and new value. If they are different,
@@ -50,6 +55,37 @@ const documentHandler = new Handler({
         }
     }
 });
+
+/**
+ * All documents in a document list must declare a field 'id'
+ */
+const documentListHandler = new Handler({
+    forHint: 'DocumentList',
+    *findChanges({path, newValue, oldValue, change, context}) {
+        // A lookup table to speed up finding documents by ID
+        const lookup = new Map(oldValue.map(document => ([document.id, document])));
+        for (const newDocument of newValue) {
+            // Check if document is new or deleted
+            if (newDocument._new) {
+                yield change({op: 'create', data: newDocument});
+            }
+            else if (newDocument._deleted) {
+                yield change({op: 'delete', where: {id: newDocument.id}});
+            }
+            else {
+                // Find document with corresponding ID and construct document path
+                let oldDocument = lookup.get(newDocument.id);
+                let documentPath = path + `[id=${newDocument.id}]`
+                yield* documentHandler.findChanges({
+                    path: documentPath,
+                    newValue: newDocument,
+                    oldValue: oldDocument,
+                    context,
+                });
+            }
+        }
+    }
+})
 
 /**
  * A Vue mixin that tracks how a prop changes over time
