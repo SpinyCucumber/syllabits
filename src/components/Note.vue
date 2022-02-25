7e413dbe75d312f352a03975fe758288017552bc
ethilty <ethilty@crimson.ua.edu>
2022-02-25 00:21:45 -0600

    Note starter file

---
+++ src/components/Note.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="note">
+        <!-- TODO -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Note',
+    props: {
+        position: { default: 'is-top' },
+        message: { default: '' },
+    }
+}
+</script>
