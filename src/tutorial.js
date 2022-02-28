f09c1e21753b861e1338ae1271ce03b36352f96e
ethilty <spinycucumber@gmail.com>
2022-02-27 19:17:42 -0600

    More tutorial

--- src/tutorial.js
+++ src/tutorial.js
@@ -21,8 +21,18 @@ export default {
             // Attach listener to handle
             handle.addEventListener('click', () => {
                 note.close();
-                advance();
+                setTimeout(advance, 1000);
             }, {once: true});
         },
+        (advance) => {
+            const note = Notes.create({ message: Translation.get('message.tutorial.dragblock'), position: 'is-right'});
+            const slot = document.querySelector('.block-dropdown .game-slot.is-holding-iamb');
+            note.attach(slot);
+            // Find slot and attach event listener
+            slot.addEventListener('mousedown', () => {
+                note.close();
+                setTimeout(advance, 1000);
+            }, {once: true});
+        }
     ]
 }
