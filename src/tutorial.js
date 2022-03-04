34da91396fb4e17373aec051d669501f8e062f7e
ethilty <spinycucumber@gmail.com>
2022-03-04 17:46:39 -0600

    Tutorial steps are objects!

--- src/tutorial.js
+++ src/tutorial.js
@@ -10,78 +10,106 @@ function showDialog(key, options = {}) {
 }
 
 export default {
+    /**
+     * The tutorial contains a list of steps.
+     * Each step is an object, which at minimum specifies a 'start' method.
+     * This method should contain logic for setting up the step, including
+     * displaying dialogs, hooking up event listeners, etc.
+     * The start method can receive the following arguments:
+     * 'advance', which is a function that advances the tutorial to the next step
+     * 'vm', which refers to the gameboard instance
+     * 'store', which is an object intended to store values that persist between steps
+     * Each step can optionally specify a 'help' property, which is a translation key
+     * that is used to show a help message to the user.
+     */
     steps: [
-        ({advance, vm, store}) => {
-            const incorrectCallback = () => {
-                if (!store.hasMissedLine) {
-                    showDialog('missedline');
-                    store.hasMissedLine = true;
+        {
+            start({advance, vm, store}) {
+                const incorrectCallback = () => {
+                    if (!store.hasMissedLine) {
+                        showDialog('missedline');
+                        store.hasMissedLine = true;
+                    }
                 }
-            }
-            // Do tutorial, which involves attaching incorrect
-            // handler to each line. The first time the player gets a line
-            // incorrect, we show a dialog explaining the mechanics.
-            for (const line of vm.$refs.lines) {
-                line.$on('incorrect', incorrectCallback);
-            }
-            showDialog('welcome', { onConfirm: advance });
+                // Do tutorial, which involves attaching incorrect
+                // handler to each line. The first time the player gets a line
+                // incorrect, we show a dialog explaining the mechanics.
+                for (const line of vm.$refs.lines) {
+                    line.$on('incorrect', incorrectCallback);
+                }
+                showDialog('welcome', { onConfirm: advance });
+            },
         },
-        ({advance}) => {
-            const note = Notes.create({ message: Translation.get('message.tutorial.openpalette'), position: 'is-right'});
-            const handle = document.querySelector('.block-dropdown .handle');
-            note.attach('.block-dropdown .handle-area');
-            // Attach listener to handle
-            handle.addEventListener('click', () => {
-                note.close();
-                setTimeout(advance, 1000);
-            }, {once: true});
-        },
-        ({advance, vm}) => {
-            const note = Notes.create({ message: Translation.get('message.tutorial.dragblock'), position: 'is-right'});
-            // Find Iamb slot
-            const picker = vm.$refs.blockDropdown.$slots.default[0].componentInstance;
-            const slot = picker.$refs.buckets.find(bucket => (bucket.holding === 'i'));
-            // Attach listener and note
-            note.attach(slot.$el);
-            slot.$once('move', () => {
-                setTimeout(() => {
+        {
+            start({advance}) {
+                const note = Notes.create({ message: Translation.get('message.tutorial.openpalette'), position: 'is-right'});
+                const handle = document.querySelector('.block-dropdown .handle');
+                note.attach('.block-dropdown .handle-area');
+                // Attach listener to handle
+                handle.addEventListener('click', () => {
                     note.close();
-                    advance();
-                }, 500);
-            });
+                    setTimeout(advance, 1000);
+                }, {once: true});
+            },
         },
-        ({advance, vm}) => {
-            const note = Notes.create({ message: Translation.get('message.tutorial.dropblock'), position: 'is-top'});
-            const slot = vm.$refs.lines[0].$refs.slots[0];
-            // Attach note and listener
-            note.attach(slot.$el);
-            const callback = (value) => {
-                if (value === 'i') {
-                    slot.$off('accept', callback);
+        {
+            start({advance, vm}) {
+                const note = Notes.create({ message: Translation.get('message.tutorial.dragblock'), position: 'is-right'});
+                // Find Iamb slot
+                const picker = vm.$refs.blockDropdown.$slots.default[0].componentInstance;
+                const slot = picker.$refs.buckets.find(bucket => (bucket.holding === 'i'));
+                // Attach listener and note
+                note.attach(slot.$el);
+                slot.$once('move', () => {
                     setTimeout(() => {
                         note.close();
                         advance();
                     }, 500);
+                });
+            },
+        },
+        {
+            help: 'dropblock',
+            start({advance, vm}) {
+                const note = Notes.create({ message: Translation.get('message.tutorial.dropblock'), position: 'is-top'});
+                const slot = vm.$refs.lines[0].$refs.slots[0];
+                // Attach note and listener
+                note.attach(slot.$el);
+                const callback = (value) => {
+                    if (value === 'i') {
+                        slot.$off('accept', callback);
+                        setTimeout(() => {
+                            note.close();
+                            advance();
+                        }, 500);
+                    }
                 }
+                slot.$on('accept', callback);
+            },
+        },
+        {
+            start({advance}) {
+                showDialog('firstblock', { onConfirm: advance });
+            },
+        },
+        {
+            help: 'firstline',
+            start({advance, vm}) {
+                const line = vm.$refs.lines[0];
+                const checkButton = line.$refs.checkButton;
+                // Attach note to check button
+                const note = Notes.create({ message: Translation.get('message.tutorial.check'), position: 'is-top'});
+                note.attach(checkButton.$el);
+                line.$once('correct', () => {
+                    note.close();
+                    advance();
+                })
+            },
+        },
+        {
+            start({advance}) {
+                showDialog('firstline', { onConfirm: advance });
             }
-            slot.$on('accept', callback);
-        },
-        ({advance}) => {
-            showDialog('firstblock', { onConfirm: advance });
-        },
-        ({advance, vm}) => {
-            const line = vm.$refs.lines[0];
-            const checkButton = line.$refs.checkButton;
-            // Attach note to check button
-            const note = Notes.create({ message: Translation.get('message.tutorial.check'), position: 'is-top'});
-            note.attach(checkButton.$el);
-            line.$once('correct', () => {
-                note.close();
-                advance();
-            })
-        },
-        ({advance}) => {
-            showDialog('firstline', { onConfirm: advance });
         },
     ]
 }
