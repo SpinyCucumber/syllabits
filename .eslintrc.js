2c8a92b0c1f24bea08dabd85fc0a3df7b6832d75
ethilty <ethilty@crimson.ua.edu>
2022-03-29 12:55:07 -0500

    Remove graphql template strings linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,14 +17,6 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'no-debugger': 'off',
-    'graphql/template-strings': [
-      'error',
-      {
-        env: 'literal',
-        projectName: 'app',
-        schemaJsonFilepath: 'node_modules/.temp/graphql/schema.json'
-      }
-    ]
   },
 
   plugins: [
