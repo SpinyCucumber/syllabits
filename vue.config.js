7606c9d5496db3b1a86847a3a3c662116751ecf5
ethilty <spinycucumber@gmail.com>
2022-03-29 23:55:50 -0500

    Custom title

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,12 @@ module.exports = {
       lintGQL: false,
     }
   },
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'SyllaBits'
+    }
+  },
   // Have to explicitly transpile vue-cli-plugin-apollo
   transpileDependencies: ['vue-cli-plugin-apollo']
 }
