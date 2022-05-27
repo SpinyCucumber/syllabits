1425d7b45932f30ddc9c5c695f5ea63652f8715f
ethilty <ethilty@crimson.ua.edu>
2022-05-27 13:39:39 -0500

    Externalized PoemEntry

--- src/components/PoemEntry.vue
+++ src/components/PoemEntry.vue
@@ -3,38 +3,33 @@
         <td><span>{{ entry.title }}</span></td>
         <td class="column-grow"><span class="has-text-grey">{{ entry.author }}</span></td>
         <td class="column-shrink">
-            <b-dropdown position="is-bottom-left" scopedSlots={
-                {
-                    trigger() {
-                        return (<b-button class="borderless" icon-left="dots-horizontal"/>);
-                    }
-                }}>
-                {this.dropdownItems()}
+            <b-dropdown position="is-bottom-left">
+                <template #trigger>
+                    <b-button class="borderless" icon-left="dots-horizontal"/>
+                </template>
+                <b-dropdown-item @click="play" class="has-text-primary">
+                    {{ $translation.get('button.poem.play') }}
+                </b-dropdown-item>,
+                <b-dropdown-item @click="share">
+                    {{ $translation.get('button.poem.share') }}
+                </b-dropdown-item>
+                <b-dropdown-item @click="edit" v-if="$store.getters.perms.has('poem.edit')">
+                    {{ $translation.get('button.poem.edit') }}
+                </b-dropdown-item>
             </b-dropdown>
         </td>
     </tr>
 </template>
 
 <script>
+import { PoemLocation } from '@/utilities'
+import { Constants } from '@/services'
+
+const LocationType = { Constants }
+
 export default {
     props: { entry: Object },
     methods: {
-        dropdownItems() {
-            let dropdownItems = [
-                <b-dropdown-item onClick={this.play} class="has-text-primary">
-                    {Translation.get('button.poem.play')}
-                </b-dropdown-item>,
-                <b-dropdown-item onClick={this.share}>
-                    {Translation.get('button.poem.share')}
-                </b-dropdown-item>
-            ];
-            if (this.$store.getters.perms.has('poem.edit')) dropdownItems = [...dropdownItems, 
-                <b-dropdown-item onClick={this.edit}>
-                    {Translation.get('button.poem.edit')}
-                </b-dropdown-item>
-            ];
-            return dropdownItems;
-        },
         play() {
             // When user selects a poem, navigate to the play menu
             const location = new PoemLocation({t: LocationType.DIRECT, p: this.entry.id}).encode();
--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -16,70 +16,10 @@
 <script>
 import { SearchPoems, SearchUsers, DeleteUser } from '@/queries'
 import { Constants, Translation } from '@/services'
-import { PoemLocation } from '@/utilities'
-import { Table, RoleTag, ChangeRole } from '@/components'
+import { Table, RoleTag, ChangeRole, PoemEntry } from '@/components'
 import NavbarView from './NavbarView'
 
-const { LocationType, Role } = Constants;
-
-const PoemEntry = {
-    props: { entry: Object },
-    methods: {
-        dropdownItems() {
-            let dropdownItems = [
-                <b-dropdown-item onClick={this.play} class="has-text-primary">
-                    {Translation.get('button.poem.play')}
-                </b-dropdown-item>,
-                <b-dropdown-item onClick={this.share}>
-                    {Translation.get('button.poem.share')}
-                </b-dropdown-item>
-            ];
-            if (this.$store.getters.perms.has('poem.edit')) dropdownItems = [...dropdownItems, 
-                <b-dropdown-item onClick={this.edit}>
-                    {Translation.get('button.poem.edit')}
-                </b-dropdown-item>
-            ];
-            return dropdownItems;
-        },
-        play() {
-            // When user selects a poem, navigate to the play menu
-            const location = new PoemLocation({t: LocationType.DIRECT, p: this.entry.id}).encode();
-            this.$router.push({name: 'Gameboard', query: { location }});
-        },
-        edit() {
-            this.$router.push({name: 'Gameboard', query: { mode: 'edit', poemID: this.entry.id }});
-        },
-        share() {
-            // Construct play URL and write to clipboard
-            const location = new PoemLocation({t: LocationType.DIRECT, p: this.entry.id}).encode();
-            const url = window.location.origin + '/#/play/' + location;
-            navigator.clipboard.writeText(url);
-            // Show success message
-            this.$buefy.toast.open({
-                message: this.$translation.get('message.poem.share'),
-                type: 'is-success',
-            })
-        }
-    },
-    render() {
-        return (
-            <tr onDblclick={this.play}>
-                <td><span>{this.entry.title}</span></td>
-                <td class="column-grow"><span class="has-text-grey">{this.entry.author}</span></td>
-                <td class="column-shrink">
-                    <b-dropdown position="is-bottom-left" scopedSlots={
-                        {
-                            trigger() {
-                                return (<b-button class="borderless" icon-left="dots-horizontal"/>);
-                            }
-                        }}>
-                        {this.dropdownItems()}
-                    </b-dropdown>
-                </td>
-            </tr>
-        )
-    },
-}
+const { Role } = Constants;
 
 const UserEntry = {
 
