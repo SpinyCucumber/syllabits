2e3cbe70c01417697a63e610afa6dd5a5473cef5
ethilty <ethilty@crimson.ua.edu>
2022-05-28 23:34:19 -0500

    Page edit/view mode transitions

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -1,37 +1,90 @@
 <template>
     <navbar-view>
         <scene type="is-aligned">
+
             <template #content-area>
                 <div class="page">
+                    <div class="toolbar">
+                        <transition-group name="list" tag="div" class="toolbar-end">
+                            <b-button
+                                v-for="button in filteredButtons"
+                                :key="button.key"
+                                v-bind="button.options"
+                                v-on="button.listeners"/>
+                        </transition-group>
+                    </div>
                     <transition name="fade">
                         <div class="page-content" v-if="page" v-html="page.content"/>
                     </transition>
                 </div>
             </template>
+
+            <template #background-area>
+                <background-image v-if="mode === 'edit'" :src="$assets.getTexture('relief1')" class="muted"/>
+            </template>
+
         </scene>
     </navbar-view>
 </template>
 
 <script>
-import { Scene } from '@/components'
+import { Scene, BackgroundImage } from '@/components'
 import { ViewPage } from '@/queries'
 import NavbarView from './NavbarView'
+import store from '@/store'
 
 export default {
+
     name: 'Page',
-    components: { Scene, NavbarView, },
+    components: { Scene, NavbarView, BackgroundImage, },
+
     props: {
         mode: { type: String, default: 'view' }, // May be 'view' or 'edit'
         path: String // May be a page path (such as 'about', 'guide', etc.) or a page ID
     },
-    apollo: {
-        page: {
-            query: ViewPage,
-            update: data => (data.page),
-            variables() {
-                return { path: this.path }
-            },
+
+    data() {
+        return {
+            page: null,
+            buttons: [
+                {
+                    key: 'edit',
+                    options: { type: 'is-dark', 'icon-left': 'pencil', },
+                    listeners: { click: this.edit },
+                    shouldShow: () => this.mode === 'view' && store.getters.perms.has('page.edit') && this.page
+                },
+                {
+                    key: 'view',
+                    options: { type: 'is-dark', 'icon-left': 'eye', },
+                    listeners: { click: this.view },
+                    shouldShow: () => this.mode === 'edit' && this.page
+                }
+            ]
         }
-    }
+    },
+
+    async created() {
+        // Load page from server
+        this.page = (await this.$apollo.query({
+            query: ViewPage, variables: { path: this.path }
+        })).data.page;
+    },
+
+    methods: {
+        edit() {
+            // Use ID to reference page in edit mode
+            this.$router.push({ name: 'Page', params: { path: this.page.id }, query: { mode: 'edit' }});
+        },
+        view() {
+            this.$router.push({ name: 'Page', params: { path: this.page.path }});
+        }
+    },
+
+    computed: {
+        filteredButtons() {
+            return this.buttons.filter(button => button.shouldShow ? button.shouldShow() : true);
+        },
+    },
+
 }
 </script>
